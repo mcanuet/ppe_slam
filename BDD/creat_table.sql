37d2a5a7624eae966b4107685fbb0f2844d06fdf
mcanuet <>
2018-10-16 16:02:13 +0200

    update BDD

--- BDD/creat_table.sql
+++ BDD/creat_table.sql
@@ -4,6 +4,36 @@
 
 
 #------------------------------------------------------------
+# Table: ligue
+#------------------------------------------------------------
+
+CREATE TABLE ligue(
+        ligue_num   Int  Auto_increment  NOT NULL ,
+        ligue_nom   Varchar (50) NOT NULL ,
+        ligue_sport Varchar (50) NOT NULL ,
+        ligue_tel   Varchar (10) NOT NULL
+	,CONSTRAINT ligue_PK PRIMARY KEY (ligue_num)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: utilisateur
+#------------------------------------------------------------
+
+CREATE TABLE utilisateur(
+        utilisateur_num          Int  Auto_increment  NOT NULL ,
+        utilisateur_identifiant  Varchar (30) NOT NULL ,
+        utilisateur_mdp          Varchar (40) NOT NULL ,
+        utilisateur_admin        Bool NOT NULL ,
+        utilisateur_responssable Bool NOT NULL ,
+        ligue_num                Int NOT NULL
+	,CONSTRAINT utilisateur_PK PRIMARY KEY (utilisateur_num)
+
+	,CONSTRAINT utilisateur_ligue_FK FOREIGN KEY (ligue_num) REFERENCES ligue(ligue_num)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
 # Table: salle
 #------------------------------------------------------------
 
@@ -13,35 +43,6 @@ CREATE TABLE salle(
         salle_places      Int NOT NULL ,
         salle_informatise Bool NOT NULL
 	,CONSTRAINT salle_PK PRIMARY KEY (salle_num)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: utilisateur
-#------------------------------------------------------------
-
-CREATE TABLE utilisateur(
-        utilisateur_num            Int  Auto_increment  NOT NULL ,
-        utilisateur_identifiant    Varchar (30) NOT NULL ,
-        utilisateur_mdp            Varchar (40) NOT NULL ,
-        utilisateur_admin          Bool NOT NULL ,
-        ligue_num                  Int NOT NULL
-	,CONSTRAINT utilisateur_PK PRIMARY KEY (utilisateur_num)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: ligue
-#------------------------------------------------------------
-
-CREATE TABLE ligue(
-        ligue_num                        Int  Auto_increment  NOT NULL ,
-        ligue_nom                        Varchar (50) NOT NULL ,
-        ligue_sport                      Varchar (50) NOT NULL ,
-        ligue_tel                        Varchar (10) NOT NULL ,
-        utilisateur_num                  Int NOT NULL ,
-        utilisateur_num_est_responssable Int NOT NULL
-	,CONSTRAINT ligue_PK PRIMARY KEY (ligue_num)
 )ENGINE=InnoDB;
 
 
@@ -57,44 +58,8 @@ CREATE TABLE reservation(
         ligue_num           Int NOT NULL ,
         salle_num           Int NOT NULL
 	,CONSTRAINT reservation_PK PRIMARY KEY (reservation_num)
+
+	,CONSTRAINT reservation_ligue_FK FOREIGN KEY (ligue_num) REFERENCES ligue(ligue_num)
+	,CONSTRAINT reservation_salle0_FK FOREIGN KEY (salle_num) REFERENCES salle(salle_num)
 )ENGINE=InnoDB;
 
-
-
-
-ALTER TABLE utilisateur
-	ADD CONSTRAINT utilisateur_ligue0_FK
-	FOREIGN KEY (ligue_num)
-	REFERENCES ligue(ligue_num);
-
-ALTER TABLE utilisateur
-	ADD CONSTRAINT utilisateur_ligue0_AK
-	UNIQUE (ligue_num);
-
-ALTER TABLE ligue
-	ADD CONSTRAINT ligue_utilisateur0_FK
-	FOREIGN KEY (utilisateur_num)
-	REFERENCES utilisateur(utilisateur_num);
-
-ALTER TABLE ligue
-	ADD CONSTRAINT ligue_utilisateur1_FK
-	FOREIGN KEY (utilisateur_num_est_responssable)
-	REFERENCES utilisateur(utilisateur_num);
-
-ALTER TABLE ligue 
-	ADD CONSTRAINT ligue_utilisateur0_AK 
-	UNIQUE (utilisateur_num);
-
-ALTER TABLE ligue 
-	ADD CONSTRAINT ligue_utilisateur1_AK 
-	UNIQUE (utilisateur_num_est_responssable);
-
-ALTER TABLE reservation
-	ADD CONSTRAINT reservation_ligue0_FK
-	FOREIGN KEY (ligue_num)
-	REFERENCES ligue(ligue_num);
-
-ALTER TABLE reservation
-	ADD CONSTRAINT reservation_salle1_FK
-	FOREIGN KEY (salle_num)
-	REFERENCES salle(salle_num);
